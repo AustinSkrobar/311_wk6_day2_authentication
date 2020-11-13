e94bf6e8ad9187ea7ad276480e5537d7864ce203
AustinSkrobar <65634947+AustinSkrobar@users.noreply.github.com>
2020-11-13 17:03:17 -0600

    homework

--- routers/auth.js
+++ routers/auth.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const authController = require('../controllers/auth')
 const router = express.Router()
+const checkJWt = require('../middleware/index')
 
 // router.post('/signup', authController.signup)
 
-router.post('/login', authController.login)
+router.post('/login', checkJWt.checkJwt, authController.login)
 
 module.exports = router
--- routers/users.js
+++ routers/users.js
@@ -2,15 +2,16 @@ const express = require('express')
 const usersController = require('../controllers/users')
 const { checkJwt } = require('../middleware')
 const router = express.Router()
+const checkJWt = require('../middleware/index')
 
 router.get('/', usersController.getAllUsers)
 
 router.get('/:id', usersController.getUserById)
 
-router.post('/', usersController.createUser)
+router.post('/', checkJWt.checkJwt, usersController.createUser)
 
-router.put('/:id', usersController.updateUserById)
+router.put('/:id', checkJWt.checkJwt, usersController.updateUserById)
 
-router.delete('/:first_name', usersController.deleteUserByFirstName)
+router.delete('/:first_name', checkJWt.checkJwt, usersController.deleteUserByFirstName)
 
 module.exports = router
